a2eeddfb24f948d318813156e7e0920e80661fd5
KMONJARD <62231623+kmonjard@users.noreply.github.com>
2021-05-19 09:50:18 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// See CSS text-decoration Property -- https://www.w3schools.com/cssref/pr_text_text-decoration.asp
+// See Generating unique ids with the UUID Package -- https://www.npmjs.com/package/uuid
